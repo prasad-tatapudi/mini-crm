957898ed0ae3b6ad6d39b7642598f1b47a6571ed
Prasad Tatapudi <tatapudiprasad325@gmail.com>
2026-04-19 09:52:25 +0530

    Initial commit - Mini CRM project

---
+++ backend/routes/leadRoutes.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+const Lead = require("../models/Lead");
+
+// Create lead
+router.post("/", async (req, res) => {
+  const lead = new Lead(req.body);
+  await lead.save();
+  res.json(lead);
+});
+
+// Get all leads
+router.get("/", async (req, res) => {
+  const leads = await Lead.find();
+  res.json(leads);
+});
+
+// Update status
+router.put("/:id", async (req, res) => {
+  const lead = await Lead.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.json(lead);
+});
+
+// Add note
+router.put("/note/:id", async (req, res) => {
+  const lead = await Lead.findById(req.params.id);
+  lead.notes.push(req.body.note);
+  await lead.save();
+  res.json(lead);
+});
+
+module.exports = router;
+// Delete lead
+router.delete("/:id", async (req, res) => {
+  await Lead.findByIdAndDelete(req.params.id);
+  res.json({ message: "Lead deleted" });
+});
